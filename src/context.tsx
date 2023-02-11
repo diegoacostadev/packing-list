a0359b934e2a7bc5858443d7130bcd66129131cf
Diego Acosta <diegoacosta211@gmail.com>
2023-02-11 15:56:10 -0300

    typed

--- src/context.tsx
+++ src/context.tsx
@@ -1,19 +1,60 @@
 import { useState, createContext, PropsWithChildren } from 'react';
-// import {
-//   createItem,
-//   filterItems,
-//   getInitialItems,
-//   removeItem,
-//   updateItem,
-// } from './lib/items';
+import {
+  createItem,
+  filterItems,
+  getInitialItems,
+  removeItem,
+  updateItem,
+} from './lib/items';
 
-const ItemsContext = createContext(null);
+type PartialItem = Omit<Partial<Item>, 'id'>;
+
+type ItemsState = {
+  items: Item[];
+  add: (name: string) => void;
+  update: (id: string, updates: PartialItem) => void;
+  remove: (id: string) => void;
+  unpackedItems: Item[];
+  packedItems: Item[];
+  markAllAsUnpacked: () => void;
+};
+
+export const ItemsContext = createContext({} as ItemsState);
 
 const ItemsProvider = ({ children }: PropsWithChildren) => {
-  // eslint-disable-next-line
-  const [items, setItems] = useState<Item[]>([]);
+  const [items, setItems] = useState<Item[]>(getInitialItems());
 
-  return <ItemsContext.Provider value={null}>{children}</ItemsContext.Provider>;
+  const add = (name: string) => {
+    const item = createItem(name);
+    setItems([...items, item]);
+  };
+
+  const update = (id: string, updates: PartialItem) => {
+    setItems(updateItem(items, id, updates));
+  };
+
+  const remove = (id: string) => {
+    setItems(removeItem(items, id));
+  };
+
+  const unpackedItems = filterItems(items, { packed: false });
+  const packedItems = filterItems(items, { packed: true });
+
+  const markAllAsUnpacked = () => {
+    return setItems(items.map((item) => ({ ...item, packed: false })));
+  };
+
+  const value: ItemsState = {
+    items,
+    add,
+    update,
+    remove,
+    unpackedItems,
+    packedItems,
+    markAllAsUnpacked
+  }
+
+  return <ItemsContext.Provider value={value}>{children}</ItemsContext.Provider>;
 };
 
 export default ItemsProvider;
